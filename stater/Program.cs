d8c53edc9a22c5d9e1fcdd16f81d5b0e0ee1104a
theo <abayateyet@gmail.com>
2025-07-28 21:38:33 +0000

    edited Readme file

--- stater/Program.cs
+++ stater/Program.cs
@@ -2,10 +2,10 @@
 // initialize variables - graded assignments 
 int currentAssignments = 5;
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
 //student Names
 string[] studentNames = new string[] { "sophia", "andrew", "emma", "logan" };
